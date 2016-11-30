9a10475f481625be6d02ef2c6d1c431c2de06a41
Michael Barnett <micalan@sas.upenn.edu>
2016-11-29 23:18:11 -0500

    extracts meridians

--- displace_2D_Map/rotAndExrtractMeridian.m
+++ displace_2D_Map/rotAndExrtractMeridian.m
@@ -1,10 +1,14 @@
-function [RGCdenisty_mmSq RFdensity_sqDeg]= rotAndExrtractMeridian(RFdensity,RGCdensity,rotDeg)
+function [RGCdenisty_mmSq,RFdensity_sqDeg]= rotAndExrtractMeridian(RFdensity,RGCdensity,rotDeg)
 
-%rotate and crop
-RF_crop   = imrotate(RFdensity,rotDeg,'crop','bilinear');
-rotRGC_crop  = imrotate(RGCdensity,rotDeg,'crop','bilinear');
+%% rotate and crop
+RF_crop   = imrotate(RFdensity,-1.*rotDeg,'crop','bilinear');
+RGC_crop  = imrotate(RGCdensity,-1.*rotDeg,'crop','bilinear');
 
-%% Get meridian fro RFdensity
-RF_midPoint = round(
+%% Get meridian from RF density rotated input image
+RF_midPoint = round(size(RF_crop,1)/2);
+RFdensity_sqDeg = RF_crop(RF_midPoint,RF_midPoint:end);
+%% Get meridian from RGC density rotated input image
+RGC_midPoint = round(size(RGC_crop,1)/2);
+RGCdenisty_mmSq = RF_crop(RGC_midPoint,RGC_midPoint:end);
 
 end
