7f7fc8746294c7b09422d8e7dee4ab685ef185e7
Michael Barnett <micalan@sas.upenn.edu>
2016-12-01 13:49:22 -0500

    Filled NaNs in map and turned makeMap into func.

---
+++ displace_2D_Map/fillNansInMap.m
@@ -0,0 +1,20 @@
+function dispMap = fillNansInMap(nanDispMap,radMM,smpPerMM)
+
+ [id1,id2]= find(isnan(nanDispMap));
+ dispMap = nanDispMap;
+ for i = 1:length(id1)
+     dot = zeros(size(nanDispMap));
+     dot(id1(i),id2(i)) = 1;
+     se = strel('disk',2,0);
+     mask = imdilate(dot,se);
+     dispMap(id1(i),id2(i)) = nanmean(nanDispMap(logical(mask)));
+ end
+     
+ %% mask the output
+ line = -radMM:1/smpPerMM:radMM;
+ [X1,X2] = meshgrid(line,line);
+ mask = sqrt(X1.^2 + X2.^2) <= radMM;
+ 
+ dispMap(~mask) = nan;
+end
+ 
--- displace_2D_Map/makeMap.m
+++ displace_2D_Map/makeMap.m
@@ -1,43 +1,28 @@
+function dispMap = makeMap(radDeg,smpPerDeg,radMM,smpPerMM,sectorAngle)
+
 % #############################################################
-% Script to reproduce Figure 1 from Turpin/Mckendrick 2015.
+% Script produces a 2D displacement image.
 % 
-% The goal is to produce a plot of the cumulative retinal
-% ganglion cell and receptive field counts as a function of
-% eccentricity (mm). 
+% The goal is to produce an image of the displacement of retinal
+% ganglion cell from receptive fields as a function of
+% eccentricity (mm) and polar angle. 
 % 
-% Everthing should be in units of mm, mm^2, or cell count
-% Conversion between units found at the end of Watson 2014
-% and fit from Drasdo 1974.
+% The output is in mm of displacement.
 % 
-% MAB 2016
+% EXAMPLE INPUT PARAMETERS:
+%  radDeg      = 20; % Radius in Degrees
+%  smpPerDeg   = 2; % Samples per Degree
+%  radMM = 5;
+%  smpPerMM = 6;
+%  sectorAngle = 6;
 %
 % Notes:
-% Curcio and Allen 1990 reference frames is the retinal feild
-%       Data loaded from .xml is in the retinal coordinate frame
-%       No extra steps neded to reference the data.
-% Drasdo 2007 usues the visual field refrerence frame
-%       Indexing the output of densityRF():
-%       ex. RFdensity = densityRf(radDeg,smpPerDeg,'OFF');
-%            midPoint = center pixel of output image
-%            Temporal retina = left of midPoint
-%            Nasal Retina    = right of midPoint
-%            Supior Retina   = above midPoint
-%            Inferior Retina = below midPoint
-% Turpin McKendrick 2015 reference frame is the visual field
-%
-% Watson 2014 reference frame is the visual field
-%
+% 
 % THIS CODE SETS THE REFERENCE FRAME TO THE RETINAL FIELD
 %
+% MAB 2016
 % #############################################################
 
-%% Generate the Receptive Field Density From the Drasdo 2007:
-% Set Parameters 
-radDeg      = 20; % Radius in Degrees
-smpPerDeg   = 2; % Samples per Degree
-radMM = 5;
-smpPerMM = 6;
-sectorAngle = 6;
 rotDegs =0:sectorAngle:360-sectorAngle;
 
 %% Presize output matrix
@@ -46,27 +31,37 @@ out_radii = 0:out_smps:radMM; % vector of radii to match Turpin code
 mapFull = nan(2*length(out_radii)-1,2*length(out_radii)-1,length(rotDegs));
 mapFullMidPt = round(length(mapFull)/2);
 
-% Obtain the Receptive field density per square degree within the sampling
-% area specified (also in degrees of visual angle)
+% Obtain the ganglion cell density per square mm within the sampling
+% area specified (also in mm)
 [RGCdensity,sampleBase_RGC_mm]= densityRGC(radMM,smpPerMM,'OFF');
 
+% Obtain the Receptive field density per square degree within the sampling
+% area specified (also in degrees of visual angle)
 [RFdensity,sampleBase_RF_deg] = densityRf(radDeg,smpPerDeg,'OFF'); % Generates a 2D Receptive Field Density plot 
 
-
+%% Generate a displacemnt vector for each sector in the retinal feild (nummber of sectors to 360/sectorAngle)
+ % This will be stored in a 3D martix with (:,:,i) slice containing a single displacemnt vector 
 for i = 1:length(rotDegs)
-    
+     % Rotate the RFdensity and the RGCdensity by theta = i*sectorAngle to extract the radial densitiy data    
     [RGCdenisty_mmSq,RFdensity_sqDeg]= rotAndExrtractMeridian(RFdensity,RGCdensity,rotDegs(i));
     
     % Convert the RF counts (and sample base) from degress / deg^2 to mm and cells/mm^2
     RFdensity_mm = convert_degSq_to_mmSq(sampleBase_RF_deg, RFdensity_sqDeg);
     sampleBase_RF_mm=convert_deg_to_mm(sampleBase_RF_deg);
     
-    
+    % Calculate the displacment 
     mapFull(mapFullMidPt,mapFullMidPt:end,i) = calcDisplacement(RFdensity_mm,sampleBase_RF_mm,RGCdenisty_mmSq,sampleBase_RGC_mm,radMM,smpPerMM,sectorAngle,'OFF');
     
+    % radially arrange the displacemnt vectors across the 3rd dim
     mapFull(:,:,i) = imrotate(mapFull(:,:,i),-1.*rotDegs(i),'crop','nearest');
     
 end
 
-dispMap = nanmean(mapFull,3);
+%% Create 2D map series of radial images
+nanDispMap = nanmean(mapFull,3);
+
+%% fill in the nans
+dispMap = fillNansInMap(nanDispMap,radMM,smpPerMM);
+
 %figure;plot(-radMM:1/smpPerMM:radMM,dispMap(31,:),'r');xlabel('eccentricity (mm)'),ylabel('displacement (mm)')
+end
