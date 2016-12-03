1579cecd15405d9a21ccf7bd4263c2b5d6553cbd
Michael Barnett <micalan@sas.upenn.edu>
2016-12-03 14:55:08 -0500

    loadMap function added

---
+++ loadMaps.m
@@ -0,0 +1,24 @@
+function [rgcPlus,dispMap] = loadMaps(octSegFile,sectorAngle)
+% load the output of Arua Tools and get the dimensions generates a matching
+% sized dispalcemnt map.
+
+%% preset sample data if no data input defined
+if isempty(octSegFile)
+    octSegFile = 'octProcessing/sample_data/11015_OD_Horizontal/11015_1_21682_result.mat';
+end
+if isempty(sectorAngle) 
+    sectorAngle=6;
+end
+
+% load OTC segmentation
+load(octSegFile)
+% vreate RGC+ thickness and sampleBase
+[rgcPlus,sampleBaseRadius] = rgcThickness(bd_pts,header);
+% Turn sambleBase into intputs for makeMap
+radMM = max(sampleBaseRadius);
+smpPerMM = radMM/(length(sampleBaseRadius)-1);
+% generate displacement map
+dispMap = makeMap(radMM,smpPerMM,sectorAngle);
+
+end
+
--- octProcessing/applyDispToRGC.m
+++ octProcessing/applyDispToRGC.m
@@ -4,11 +4,16 @@ xCent = round(size(rgcThickness,2)/2);
 yCent = round(size(rgcThickness,1)/2);
 
 %%for a point in rcgThickness, look up in dispMap the displacement.
-
+for y = 1:size(rgcThickness,1)
+    for x = 1:size(rgcThickness,2)
+        
 %% find the x y coord of the new point that is disp mm from starting point towards image center
 
 %% iterativly nansum
 
 %% outputs
+    end
+end
+
 
 end
