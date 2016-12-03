de19eca6301379e3c3ead863dd59ae67b5285789
Michael Barnett <micalan@sas.upenn.edu>
2016-12-02 22:42:57 -0500

    added outline to code applyDispToRGC

---
+++ octProcessing/applyDispToRGC.m
@@ -0,0 +1,14 @@
+function correctedRGC = applyDispToRGC(rgcThickness,dispMap)
+
+xCent = round(size(rgcThickness,2)/2);
+yCent = round(size(rgcThickness,1)/2);
+
+%%for a point in rcgThickness, look up in dispMap the displacement.
+
+%% find the x y coord of the new point that is disp mm from starting point towards image center
+
+%% iterativly nansum
+
+%% outputs
+
+end
