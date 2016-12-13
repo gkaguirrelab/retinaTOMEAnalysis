a0d3a5d439c6848b2a30ae092eb7cfaf40f068f4
Michael Barnett <micalan@sas.upenn.edu>
2016-12-13 11:24:04 -0500

    check radial

--- displacementMaps/validationTools/checkSingleRadial.m
+++ displacementMaps/validationTools/checkSingleRadial.m
@@ -1,6 +1,17 @@
-function checkSingleRadial(map,radialDeg)
+function checkSingleRadial(map,radialRotDeg,sizeMM,smpPerMM)
 
+%Create sample base in MM based on map size and sample rate
+sampleBase=0:1/smpPerMM:sizeMM;
 
- rotMap   = imrotate(map,-1.*radialDeg,'crop','nearest');
- 
- 
+rotMap   = imrotate(map,-1.*radialRotDeg,'crop','nearest');
+
+center = round(size(rotMap,1));
+
+radial = rotMap(center,center:end);
+
+newPos = sampleBase - radial;
+
+diffRadial = diff(
+
+figure;
+subplot(
