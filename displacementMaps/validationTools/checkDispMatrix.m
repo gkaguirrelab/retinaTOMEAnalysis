e56e046ea39867c0b6a760d8cad02777bdf809c5
Michael Barnett <micalan@sas.upenn.edu>
2016-12-15 14:13:27 -0500

    working

--- displacementMaps/validationTools/checkDispMatrix.m
+++ displacementMaps/validationTools/checkDispMatrix.m
@@ -1,7 +1,7 @@
-function checkDispMatrix(radialRotDeg,sizeMM,smpPerMM,sectorAngle)
+function diffRadial = checkDispMatrix(sizeMM,smpPerMM,sectorAngle)
 
 
-map = makeMap(sizeMM,smpPerMM,sectorAngle)
+map = makeMap(sizeMM,smpPerMM,sectorAngle);
 
 %Create sample base in MM based on map size and sample rate
 sampleBase=0:1/smpPerMM:sizeMM;
@@ -12,28 +12,31 @@ newPos = sampleBaseSurf - map;
 diffRadial = diff(newPos,1,2);
 
 figure;
-subplot(2,2,1)
-imagesc(map)
-subplot(2,2,2)
-plot(sampleBase,radial)
-xlabel('Eccentricity (mm)')
-ylabel('Displacement (mm)')
-title('Displacement Along Radial')
-axis square
-subplot(2,2,3)
-plot(sampleBase,newPos)
-xlabel('Eccentricity (mm)')
-ylabel('Displaced Location (mm)')
-title('New Position Along Radial')
-hline =refline(1,0)
-hline.Color = 'k'
-hline.LineStyle = '--'
-axis square
-subplot(2,2,4)
-plot(diffRadial);
-ylabel('Diff (mm)')
-title('Diff of Displacement Along Radial')
-axis square
+surf(diffRadial);
+
+% figure;
+% subplot(2,2,1)
+% imagesc(map)
+% subplot(2,2,2)
+% plot(sampleBase,radial)
+% xlabel('Eccentricity (mm)')
+% ylabel('Displacement (mm)')
+% title('Displacement Along Radial')
+% axis square
+% subplot(2,2,3)
+% plot(sampleBase,newPos)
+% xlabel('Eccentricity (mm)')
+% ylabel('Displaced Location (mm)')
+% title('New Position Along Radial')
+% hline =refline(1,0)
+% hline.Color = 'k'
+% hline.LineStyle = '--'
+% axis square
+% subplot(2,2,4)
+% plot(diffRadial);
+% ylabel('Diff (mm)')
+% title('Diff of Displacement Along Radial')
+% axis square
 
 
 end
