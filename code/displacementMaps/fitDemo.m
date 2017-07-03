9e8b48fc0987b3c402fdcaadd2e8f3fae8af95aa
Michael Barnett <micalan@sas.upenn.edu>
2017-07-02 21:05:22 -0400

    created fitDemo.m

---
+++ code/displacementMaps/fitDemo.m
@@ -0,0 +1,29 @@
+%demo of fit functions
+
+%% Fit Functions
+%input angle
+angle = 45;
+% fit the RGC density
+[ecc_mm,RGCfit] = fitRGCdensity(angle,6);
+% fit the RF density -- need to convert mm to deg
+RFfit = fitRFdensity(convert_mm_to_deg(ecc_mm),angle);
+
+%% Plot 
+figure;
+plot(ecc_mm,exp(RGCfit(log(ecc_mm))))
+hold on
+plot(ecc_mm,RFfit(convert_mm_to_deg(ecc_mm)))
+
+%% Sum and plot
+
+RGCplot = exp(RGCfit(log(ecc_mm)));
+RGCplot(1) = 0;
+sum_RGCfit = cumsum(RGCplot);
+
+sum_RFfit = cumsum(RFfit(convert_mm_to_deg(ecc_mm)));
+
+figure;
+plot(convert_mm_to_deg(ecc_mm),sum_RGCfit)
+hold on
+plot(convert_mm_to_deg(ecc_mm),sum_RFfit)
+
