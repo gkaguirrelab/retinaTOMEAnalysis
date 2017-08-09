f5d2bf3d67a9776b43146dd04506f4ca060f774f
gkaguirre <gkaguirre@me.com>
2017-08-09 18:31:16 -0400

    Implemented as a loop across meridia, and using the new Watson RF and Curcio RGC functions
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/displacementMaps/testEmpiricalCumulativeConvergance.m
+++ code/displacementMaps/testEmpiricalCumulativeConvergance.m
@@ -1,135 +1,90 @@
 % #############################################################
-% Script to "reproduce" (get close) Figure 1 from Turpin/Mckendrick 2015.
-% 
 % The goal is to produce a plot of the cumulative retinal
 % ganglion cell and receptive field counts as a function of
-% eccentricity (mm). 
-% 
-% Everthing should be in units of mm, mm^2, or cell count
-% Conversion between units found at the end of Watson 2014
-% and fit from Drasdo 1974.
-% 
+% eccentricity (mm).
+%
 % MAB 2016
 %
 % Notes:
-% Curcio and Allen 1990 reference frames is the retinal feild
-%       Data loaded from .xml is in the retinal coordinate frame
-%       No extra steps neded to reference the data.
-% Drasdo 2007 usues the visual field refrerence frame
-%       Indexing the output of densityRF():
-%       ex. RFdensity = densityRf(radDeg,smpPerDeg,'OFF');
-%            midPoint = center pixel of output image
-%            Temporal retina = left of midPoint
-%            Nasal Retina    = right of midPoint
-%            Supior Retina   = above midPoint
-%            Inferior Retina = below midPoint
-% Turpin McKendrick 2015 reference frame is the visual field
-%
-% Watson 2014 reference frame is the visual field
-%
-% THIS CODE SETS THE REFERENCE FRAME TO THE RETINAL FIELD
+% THIS CODE SETS THE REFERENCE FRAME TO THE RETINA
 %
 % #############################################################
 
+% housekeeping
 clc
+clear vars
 close all
 
-%% Load the RGC Density Data from Curcio and Allen 1990:
-% Curcio and Allen obtained measurements of the denisty of all RGC classes
-% within 6 human retinas at a set of positions relative to the fovea. These
-% data were provided online in 2013.
-curcio_data = fullfile([getpref('octAnalysisForTOME','LocalDataPath') , '/Curcio/curcio_4meridian.mat']);
-data=load(curcio_data);
-data=data.data;
+% Define the meridia for which we will make the calculation
+meridia = [0 90 180 270];
+meridiaNames = {'nasal' 'superior' 'temporal' 'inferior'};
+displacementPointDeg = [17 17 11 17];
+midgetFracAdjust = [0.8230 1.0375 0.8453 1.1791];
 
-% Here we distribute the contents of the Curcio measurements across some
-% variables:
-%  ecc_mm - defines the distance in mm from the fovea along each of the
-%           radials for which we have denisty measurements.
-%  temp / sup / nasal / inferior - RGC density (counts / mm^2) at each
-%           position along each radial. The labels correspond to retinal
-%           coordinate frame (i.e., nasal refers to the nasal retina).
-%           This can be verified by observing that there is an interruption
-%           in the count data for the nasal meridian corresponding to the
-%           blind spot.
-supportPosMm  = data(:,1);
-supportPosDeg = convert_mm_to_deg(supportPosMm);
+% Loop over meridia
+for ii = 1:length(meridia)
+    
+    % Load the RGC Density Data from Curcio and Allen 1990:
+    [ midget_rgcDensity_degSq, supportPosDeg ] = getCurcioMidgetRGCDensityByEccen( meridia(ii) );
+    
+    % Plot the raw RGC density data
+    plot(supportPosDeg,midget_rgcDensity_degSq,'.r')
+    xlabel('eccentricity [deg]');
+    ylabel('density [cells / deg2]');
+    title('midget RGC density derived from Curcio and Allen 1990 (w spline fit)');
+    hold on
+    
+    % Adjust the midget RGC denisty by the fudge factor
+    midget_rgcDensity_degSq = midget_rgcDensity_degSq * midgetFracAdjust(ii);
 
-rgcDensity_degSq_temporal = convert_mmSq_to_degSq(supportPosDeg,data(:,2));
-rgcDensity_degSq_superior = convert_mmSq_to_degSq(supportPosDeg,data(:,4));
-rgcDensity_degSq_nasal    = convert_mmSq_to_degSq(supportPosDeg,data(:,6));
-rgcDensity_degSq_inferior = convert_mmSq_to_degSq(supportPosDeg,data(:,8));
+    % Fit a spline to the RGC density data
+    splineFunctionSuperior = fit(supportPosDeg,midget_rgcDensity_degSq,'smoothingspline', 'Exclude',find(isnan(midget_rgcDensity_degSq)),'SmoothingParam', 1);
+    
+    % Add the spline fit to the plot
+    plot(0:0.1:max(supportPosDeg), splineFunctionSuperior(0:0.1:max(supportPosDeg)),'-k');
+    hold off
+    
+    % Switch to equal eccentricity sampling between 0 and 20 degrees
+    supportPosDeg = 0:0.01:30;
+    
+    % Now get the midget RF density
+    midgetRFDensity_degSq = getWatsonMidgetReceptiveFieldDensityByEccen(supportPosDeg, 90);
+        
+    % Plot the midget RF density
+    figure
+    plot(supportPosDeg,midgetRFDensity_degSq,'-b')
+    xlabel('eccentricity [deg]');
+    ylabel('density [mRF / deg2]');
+    title('midget RF density from Watson 2014 eq 8');
 
+    % Calculate the counts per growing ring
+    
+    % Watson 2*pi*r correction
+    ringArea = [0,diff(supportPosDeg.^2 * pi)];
+    
+    countPerRingRF = cumsum(midgetRFDensity_degSq.*ringArea);
+    countPerRingRGC = cumsum(splineFunctionSuperior(supportPosDeg).*ringArea');
+    
+    % Plot the RGC and RF density data, within growing areas
+    figure
+    plot(supportPosDeg,countPerRingRGC,'-k')
+    xlabel('eccentricity [deg]');
+    ylabel('counts [cells per sector]');
+    hold on
+    plot(supportPosDeg,countPerRingRF,'-b')
+    title([meridiaNames{ii} ' - counts per ring, mRGC black, mRF blue']);
+    hold off
+    
+    % Calculate the RF / RGC ratio within the 20-30 degree range
+%     idx = find((supportPosDeg > 20) .* (supportPosDeg < 30));
+%     mean(countPerRingRF(idx) ./ countPerRingRGC(idx)')
 
-% Adjust the RGC counts to reflect the fraction that is midget cells
-midgetFraction = midgetFractionByEccen(supportPosDeg);
-midget_rgcDensity_degSq_temporal = rgcDensity_degSq_temporal.* midgetFraction;
-midget_rgcDensity_degSq_superior = rgcDensity_degSq_superior.* midgetFraction;
-midget_rgcDensity_degSq_nasal = rgcDensity_degSq_nasal.* midgetFraction;
-midget_rgcDensity_degSq_inferior = rgcDensity_degSq_inferior.* midgetFraction;
-
-% Plot the raw RGC density data
-plot(supportPosDeg,midget_rgcDensity_degSq_superior,'.r')
-hold on
-plot(supportPosDeg,midget_rgcDensity_degSq_superior,'-k')
-xlabel('eccentricity [deg]');
-ylabel('density [cells / deg2]');
-hold off
-
-% Fit a spline to the RGC density data
-splineFunctionSuperior = fit(supportPosDeg,midget_rgcDensity_degSq_superior,'smoothingspline', 'Exclude',find(isnan(midget_rgcDensity_degSq_superior)),'SmoothingParam', 1);
-
-% Switch now to equal eccentricity sampling
-supportPosDeg = 0:1:20;
-
-
-%% Now get the RF density (reflecting the midget component)
-
-% Receptive field density (Watson 2014, eq XX)
-superiorRFDensity       = 2*(14804.6) * ( 0.9935*(1+supportPosDeg/(1.035)).^-2+(1-0.9935)*exp(-1*supportPosDeg/16.35));
-
-% Adjust to have just the midget fraction component of the RF density
-% (Watosn 2014 eq YY)
-superiorRFDensity       = (superiorRFDensity .* (0.8928*(1+supportPosDeg./41.03).^-1));
-
-% Plot the raw RF density calculation
-figure
-plot(supportPosDeg,superiorRFDensity,'.r')
-hold on
-plot(supportPosDeg,superiorRFDensity,'-k')
-xlabel('eccentricity [deg]');
-ylabel('density [RF / deg2]');
-hold off
-
-% Watson 2*pi*r correction I AM NOT SURE 
-ringArea = [0,diff(supportPosDeg.^2 * pi)];
-
-
-% Plot the RGC counts per ring
-figure
-plot(supportPosDeg,splineFunctionSuperior(supportPosDeg).*ringArea','.r')
-hold on
-plot(supportPosDeg,splineFunctionSuperior(supportPosDeg).*ringArea','-k')
-xlabel('eccentricity [deg]');
-ylabel('RGC counts per ring');
-hold off
-
-
-countPerRingRF = cumsum(superiorRFDensity.*ringArea);
-countPerRingRGC = cumsum(splineFunctionSuperior(supportPosDeg).*ringArea');
-
-% Plot the RGC and RF density data, within growing areas
-figure
-plot(supportPosDeg,countPerRingRGC,'.r')
-hold on
-plot(supportPosDeg,countPerRingRGC,'-k')
-xlabel('eccentricity [deg]');
-ylabel('counts [cells per sector]');
-plot(supportPosDeg,countPerRingRF,'.r')
-hold on
-plot(supportPosDeg,countPerRingRF,'-b')
-hold off
-
-
-
+    % Calculate the RF / RGC ratio at the theoretical displacement point
+    idx = find(supportPosDeg == displacementPointDeg(ii));
+    mean(countPerRingRF(idx) ./ countPerRingRGC(idx)')
+        
+    pause
+    close all
+    
+end % loop over meridia
 
