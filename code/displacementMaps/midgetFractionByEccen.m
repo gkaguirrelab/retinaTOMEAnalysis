7ee81b4103b7b5d605e31df8758fe38313270e55
gkaguirre <gkaguirre@me.com>
2017-08-09 18:30:53 -0400

    Comments and cleanup
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/displacementMaps/midgetFractionByEccen.m
+++ code/displacementMaps/midgetFractionByEccen.m
@@ -1,7 +1,10 @@
 function midgetFraction = midgetFractionByEccen(supportPosDeg)
-%midgetFractionByEccen -- This function returns the fraction of midget 
+% midgetFractionByEccen -- This function returns the fraction of midget 
 % retinal ganglion cells in relation to the entire count of RGCs at a given
-% distance (in deg) from the fovea. The equation is taken from Watson JoV 2014.   
+% distance (in deg) from the fovea.
+%
+% The equation is taken from Watson JoV 2014 (eq 7, plotted in fig 8), 
+% which Watson took from Drasdo et al 2007.
 %
 % Inputs: 
 %   supportPosDeg  = Sample positions along a meridian in degrees.
@@ -17,6 +20,7 @@ function midgetFraction = midgetFractionByEccen(supportPosDeg)
 % Constants from Watson 2014.
 f0 = 0.8928;
 rm = 41.03;
+
 % Equation for mRGC fraction.
 midgetFraction = f0.*(1+(supportPosDeg./rm)).^-1;
 end 
