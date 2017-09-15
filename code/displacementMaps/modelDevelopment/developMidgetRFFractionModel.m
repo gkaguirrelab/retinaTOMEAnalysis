0fd6faac33716b26b8e096869730fd153faac18b
gkaguirre <gkaguirre@me.com>
2017-09-15 10:01:39 -0400

    New model approach to midgetRF density
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/displacementMaps/modelDevelopment/developMidgetRFFractionModel.m
+++ code/displacementMaps/modelDevelopment/developMidgetRFFractionModel.m
@@ -1,58 +1,53 @@
 function [ fitParams, figHandle ] = developMidgetRFFractionModel( varargin )
-% developMidgetRGCFractionModel( varargin )
+% developMidgetRFFractionModel( varargin )
 %
-% Our goal is to be able to derive the fraction of RGCs that are midget
-% RGCs at a point in the retina, but without reference to the polar angle
-% or eccentricity of the measurement.
+% Examine the relationship between cone density and midget
+% receptive field density. Note that the mRF density reflects both the
+% ratio of convergence of cones onto retinal ganglion cells, as well as the
+% fraction of retinal ganglion cells at any one point that are midgets.
 %
-% To solve this problem, we first take the cumulative of the RGC density
-% function along a meridian. We then express this function as a proportion
-% of the cumuluative density value at any point to the cumulative denisty
-% value at a specific eccentricity location (e.g., at 30 degrees). Thus,
-% the proportional cumulative density of RGCs is equal to unity (1) at the
-% reference eccentricity. The value of the reference eccentricity should be
-% sufficiently large so that the RGC density function is in the
-% monotonically decreasing phase.
+% Judging from the work of Watson (2014), the ratio at the fovea, would be
+% expected to be 1.786 : 1, as there is believed to be a 2:1 cone:RGC
+% ratio, and the midget fraction is modeled as being 0.8928. However, a
+% higher value (1.9) was observed to best fit the data across meridians.
 %
-% Next, the log10 of the proportional, cumulative RGC density function is
-% related to the predicted midget fraction for that location, as derived by
-% eq 7 of Watson 2014 JOV (which itself is taken from Drasdo 2007). We
-% observe that a three-component reciprocal function fits this relationship
-% well.
+% For each meridian, We first load the Curcio cone density measurements.
+% At each of the sampled eccentricity values, we obtain the midgetRF
+% density as provided by Eq 8 of Watson 2014.
 %
-% We obtain the fit parameters for this relationship for each of the four
-% meridians for which we have data, and take the median fit parameters
-% across merdians. This gives us a functional form that can be used to
-% convert RGC density to midget RGC density knowing only the proportion of
-% the cumulative RGC density function at the location for which the
-% measurement was made. If we term this proportion value x, then the 
-% relationship is:
+% We observe that the ratio of midgetRF density to cone density as a
+% function of log10 cone density is sigmoidal. To support generalization
+% across datasets, we express the x-axis as log10 of the proportion of
+% maximum cone density observed in the fovea. We then fit this relationship
+% with the function:
 %
-%   midgetFraction =  f0 - [(1./(a+(b.* log10(x) )))+c]
+%	mRFtoConeDensityRatio = minRatio+(maxRatio-minRatio)./(1+(x./inflect).^slope)
 %
-% where f0 is the maximum midget fraction found at the fovea, given by
-% Watson / Drasdo as 0.8928.
+% where maxRatio and minRatio are locked at the values of 2 and 0, respectively.
+%
+% We calculate the fit of this function across each of the four meridians,
+% then take the median of the two free fit parameters (slope and inflect)
+% and return these.
+%
 %
 % OUTPUT
-%   fitParams - The median values of [a b c] across the four merdians
+%   fitParams - The median values of [slope inflect] across the four merdians
 %   figHandle - Handle to a figure showing the fits. Empty if no plotting
 %      requested.
 %
 % OPTIONS
-%   referenceEccen - the reference eccentricity for the proportion of
-%       the cumulative RGC density. The proportion function will have a
-%       value of unity at this point.
-%   supportResolutionDegrees - the resolution (in degrees) for which the 
-%       calculations are made.
-%   supportEccenMaxDegrees - the maximum eccentricity used for modeling
+%   supportEccenMaxDegrees - the maximum eccentricity used for modeling.
+%       This value should be sufficiently high so that we are in the
+%       asymptote range of cone density
 %   meridianNames - Cell array of the text string names of the meridia
 %   meridianAngles - Polar angle values assigned to the meridians
-%   watsonEq8_f0 - The midget fraction assigned to the fovea
-%   watsonEq8_rm - decay parameter controlling the midget fraction function
-%   recipFitStartPoint - initial values used for the fit of the
-%       three-parameter reciprocal function. It is critical that the second
-%       parameter (b) has a negative start point to fit this inverted
-%       reciprocal.
+%   minRatio - The minimum value of the mRF:cone density ratio. Set to zero
+%       as the functions appear to asymptote close to this value.
+%   maxRatio - The maximuim value of the mRF:cone density ratio. Set to
+%       1.9 following the logic outlined above.
+%   logitFitStartPoint - initial values used for the slope and inflection
+%       point parameters of the logisic fit. Informed by examination of
+%       typical data.
 %   verbose - Controls text output to console
 %   makePlots - Do we make a figure?
 
@@ -64,13 +59,12 @@ p = inputParser;
 
 % Optional anaysis params
 p.addParameter('referenceEccen',30,@isnumeric);
-p.addParameter('supportResolutionDegrees',0.01,@isnumeric);
-p.addParameter('supportEccenMaxDegrees',30,@isnumeric);
+p.addParameter('supportEccenMaxDegrees',60,@isnumeric);
 p.addParameter('meridianNames',{'Nasal' 'Superior' 'Temporal' 'Inferior'},@iscell);
 p.addParameter('meridianAngles',[0, 90, 180, 270],@isnumeric);
-p.addParameter('watsonEq8_f0',0.8928,@isnumeric);
-p.addParameter('watsonEq8_rm',39,@isnumeric);
-p.addParameter('recipFitStartPoint',[3 -8 0],@isnumeric);
+p.addParameter('minRatio',0,@isnumeric);
+p.addParameter('maxRatio',1.9,@isnumeric);
+p.addParameter('logitFitStartPoint',[3,-1],@isnumeric);
 
 % Optional display params
 p.addParameter('verbose',true,@islogical);
@@ -82,14 +76,11 @@ p.parse(varargin{:})
 
 %% House keeping and setup
 
-% Define a three-parameter reciprocal function that will be used to fit the
-% modeled relationship
-recipFunc = fittype('(1./(a+(b.*x)))+c','independent','x','dependent','y');
-
-% Define the regular-spaced eccentricity support over which we will model
-% the anatomical retinal functions
-regularSupportPosDeg = ...
-    0:p.Results.supportResolutionDegrees:p.Results.supportEccenMaxDegrees;
+% Define a four-parameter logistic function that will be used to fit the
+% modeled relationship. Two of the parameters (max and min asymptote) are
+% locked by the passed parameter
+logisticFunc = fittype( @(slope,inflect,minRatio,maxRatio,x) minRatio+(maxRatio-minRatio)./(1+(x./inflect).^slope), ...
+    'independent','x','dependent','y','problem',{'minRatio','maxRatio'});
 
 % Prepare a figure if requested
 if p.Results.makePlots
@@ -110,73 +101,54 @@ for mm = 1:length(p.Results.meridianAngles)
     nativeSupportPosDeg = nativeSupportPosDeg(isvalididx);
     coneDensitySqDeg = coneDensitySqDeg(isvalididx);
     
-    % Fit a spline to the RF density data
-    coneDensityFit = fit(nativeSupportPosDeg',coneDensitySqDeg','smoothingspline', 'Exclude',find(isnan(coneDensitySqDeg)),'SmoothingParam', 1);
-    
-    % Obtain the cumulative cone function
-    cone_ringcount = calcCumulative(regularSupportPosDeg,coneDensityFit(regularSupportPosDeg)');
-    
-    % Find the index position in the regularSupportPosDeg that is as close
-    % as possible to the referenceEccen
-    refPointIdx= ...
-        find((regularSupportPosDeg-p.Results.referenceEccen)== ...
-        min(abs(regularSupportPosDeg-p.Results.referenceEccen)));
-    % Calculate a proportion of the cumulative cone density counts, relative
-    % to the reference point (which is assigned a value of unity)
-    propCone_ringcount=coneDensityFit(regularSupportPosDeg)'./max(coneDensityFit(regularSupportPosDeg)');
-    
-    % Because we are going to be working with a log transform, set any zero
-    % proportion values to the minimum, non-zero proportion value
-    zeroPoints=find(propCone_ringcount==0);
-    if ~isempty(zeroPoints)
-        propCone_ringcount(zeroPoints)=min(propCone_ringcount(find(propCone_ringcount~=0)));
-    end
-    
     % calculate the mRF density at these eccentricity locations using
     % Watson equation 8.
-    [ midgetRFDensitySqDeg ] = getWatsonMidgetReceptiveFieldDensityByEccen(regularSupportPosDeg, p.Results.meridianAngles(mm));
-
-    % obtain the cumulative mRF density function
-    mRF_ringcount = calcCumulative(regularSupportPosDeg,midgetRFDensitySqDeg);
+    [ midgetRFDensitySqDeg ] = getWatsonMidgetReceptiveFieldDensityByEccen(nativeSupportPosDeg, p.Results.meridianAngles(mm));
     
-    % calculate the ratio of mRFs to cones at each support position
-    mRFtoConeRatioByEccen = midgetRFDensitySqDeg./coneDensityFit(regularSupportPosDeg)';
+    % Remove nans and points beyond the modeled eccentricity bound
+    isvalididx=find(~isnan(midgetRFDensitySqDeg).*~isnan(coneDensitySqDeg) .* (nativeSupportPosDeg < p.Results.supportEccenMaxDegrees) );
+    nativeSupportPosDeg = nativeSupportPosDeg(isvalididx);
+    coneDensitySqDeg = coneDensitySqDeg(isvalididx);
+    midgetRFDensitySqDeg = midgetRFDensitySqDeg(isvalididx);
     
-%     
-%     % Fit the reciprocal model that relates log10(proportionRGC) to midget
-%     % fraction. First, define a weight function to lock the f0 value
-%     weights=ones(1,length(propCone_ringcount));
-%     weights(1)=1e5;
-%     
-%     % Perform the fit and save the param values.
-%     % The x values are (-1) * log10(proportionRGC)
-%     % The y values are the f0 value minus the midget fraction
-%     recipFit= ...
-%         fit(log10(propCone_ringcount'),p.Results.watsonEq8_f0-midgetFractionByEccen', ...
-%         recipFunc,'Weights',weights,'StartPoint',p.Results.recipFitStartPoint );
-%     fitParams(mm,1)=recipFit.a;
-%     fitParams(mm,2)=recipFit.b;
-%     fitParams(mm,3)=recipFit.c;
+    % Define the ratio function.
+    midgetRFtoConeRatio = (midgetRFDensitySqDeg ./ coneDensitySqDeg)';
+    
+    % Define the x-axis as the log10 of the proportion of max cone density
+    x = log10(coneDensitySqDeg ./ max(coneDensitySqDeg))';
+    
+    % Perform the logistic fit. Note that the max and min asymptote are
+    % pinned by the passed parameters
+    logitFit = fit(x,midgetRFtoConeRatio,logisticFunc, ...
+        'problem',{p.Results.minRatio, p.Results.maxRatio}, ...
+        'StartPoint',p.Results.logitFitStartPoint, ...
+        'Lower',[0,-2],'Upper',[10,0] );
+    
+    % Distribute the logitFit params to a loop variable
+    fitParams(mm,1)=logitFit.slope;
+    fitParams(mm,2)=logitFit.inflect;
     
     % Add the data to the figure
     if p.Results.makePlots
-        plot((propCone_ringcount),(mRFtoConeRatioByEccen),'.','color',[.8 .8 .8])
+        plot(x,midgetRFtoConeRatio,'.','Color',[.7 .7 .7]);
         hold on
-%        ylim([0 3]);
-        xlabel('log10 proportion cumulative RGC density count');
-        ylabel('midget fraction');
+        legend off
+        xlabel('log10 proportion max cone density');
+        ylabel('mRF density : cone density');
+        title([p.Results.meridianNames{mm} ' meridian']);
     end % if we are plotting
     
 end % loop over meridians
 
 % Now calculate the median parm values across the meridians and add a fit
 % line to the plot
-% fitParams=median(fitParams);
-% 
-% if p.Results.makePlots
-%     xFit=logspace(-12,1,100);
-%     plot( log10(xFit),p.Results.watsonEq8_f0-recipFunc(fitParams(1),fitParams(2),fitParams(3),log10(xFit)),'xr')
-% end
+fitParams=median(fitParams);
+
+if p.Results.makePlots
+    xFit= -2:.01:0;
+    plot( xFit, ...
+        logisticFunc(fitParams(1), fitParams(2), p.Results.minRatio, p.Results.maxRatio, xFit),'-r')
+end
 
 
 end % function
