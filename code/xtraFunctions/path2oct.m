0cf0f09c32bcb91381dcadc5a629279e5ae8ef3f
gkaguirre <gkaguirre@me.com>
2018-10-20 17:38:56 -0400

    Cleaning up
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/xtraFunctions/path2oct.m
+++
@@ -1,5 +0,0 @@
-function outPath = path2oct
-path = pwd;
-pos = strfind(path,'octAnalysisForTOME');
-outPath = path(1:pos+17);
-end
--- xOld/liminalCode/README.txt
+++
@@ -1,3 +0,0 @@
-LIMINAL CODE
-
-This code is currently not being used in an approach, is not well commented, or is perhaps defunct.
