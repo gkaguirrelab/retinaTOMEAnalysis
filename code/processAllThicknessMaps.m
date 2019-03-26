4cc764de78876dd805b6dd40aae014c18967d6e0
Min Chen <dontminchenit@gmail.com>
2019-03-26 11:03:53 -0400

    More generic dir locations

--- code/processAllThicknessMaps.m
+++ code/processAllThicknessMaps.m
@@ -1,16 +1,13 @@
 addpath('.\SupportFunctions');
 
-%baseDir = '/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/';
-baseDir = 'C:\Users\dontm\Dropbox (Aguirre-Brainard Lab)'
+baseDir = '/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/';
 saveDir = '/Users/aguirre/Documents/MATLAB/projects/rgcPopulationModel/data/';
-saveDir = 'C:\Users\dontm\Downloads\temp';
 %
 %input directories
 inputVolandSegDir = fullfile(baseDir,'AOSO_analysis','OCTExplorerSegmentationData');
 
 %output directories
 thicknessMapSaveDir = fullfile(baseDir,'AOSO_analysis','2DThicknessMapsAllLayers_MinChenMontage');
-thicknessMapSaveDir = 'C:\Users\dontm\Downloads\temp';
 rgcMapSavePath = fullfile(saveDir,'rgcIplThicknessMap.mat');
 resultTableFileName = fullfile(saveDir,'data/octRGCResultTable.csv');
  
