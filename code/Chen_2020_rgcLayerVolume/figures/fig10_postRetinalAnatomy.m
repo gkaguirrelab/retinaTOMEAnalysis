94b2e9b04cf900c003f3881cadfa34f068871903
gkaguirre <gkaguirre@me.com>
2020-05-19 16:27:26 -0400

    CI around correlation values
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/Chen_2020_rgcLayerVolume/figures/fig10_postRetinalAnatomy.m
+++ code/Chen_2020_rgcLayerVolume/figures/fig10_postRetinalAnatomy.m
@@ -26,9 +26,14 @@ anatComparisonTable = join(anatMeasuresTable,comboTable,'Keys','AOSO_ID');
 %% Report the correlation of optic chiasm volume with some measures
 measureSet = {'gcMeanThick','meanFitGCVol','meanAdjustedGCVol'};
 y = anatComparisonTable.Optic_Chiasm;
+nBoots = 1000;
 for ii = 1:length(measureSet)
     [R,P] = corrcoef(y,anatComparisonTable.(measureSet{ii}));
-    str = sprintf(['Correlation of optic chiasm volume with ' measureSet{ii} ' = %2.2f, p = %2.5f \n'],R(1,2),P(1,2));
+    for bb = 1:nBoots
+        bootSamp = randsample(length(y),length(y),true);
+        bootR(bb) = corr(y(bootSamp),anatComparisonTable.(measureSet{ii})(bootSamp));
+    end
+    str = sprintf(['Correlation of optic chiasm volume with ' measureSet{ii} ' = %2.2f ± %2.2f (sem), p = %2.5f \n'],R(1,2),std(bootR),P(1,2));
     fprintf(str);
 end
 
