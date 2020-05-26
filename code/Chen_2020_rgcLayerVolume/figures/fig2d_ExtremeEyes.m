50119141d81359fe1ff6e458fdd3939d6703790d
gkaguirre <gkaguirre@me.com>
2020-05-26 16:14:37 -0400

    Plot some equal volume cylinders
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/Chen_2020_rgcLayerVolume/figures/fig2d_ExtremeEyes.m
+++ code/Chen_2020_rgcLayerVolume/figures/fig2d_ExtremeEyes.m
@@ -14,6 +14,9 @@ G = calcRetinaFieldPoint( sceneGeometry.eye, a0);
 plotOpticalSystem('surfaceSet',sceneGeometry.refraction.retinaToCamera,'addLighting',true,'rayPath',rayPath,'outputRay',outputRay);
 plot3(X(:,1),X(:,2),X(:,3),'.b');
 
+bigEyeRadiusMm = mean(vecnorm((X-mean(X))')');
+
+
 sceneGeometry = createSceneGeometry('axialLength',21.79,'sphericalAmetropia',3.875);
 for ii=1:length(theta)
     x=radius*cos(theta(ii));
@@ -23,12 +26,57 @@ end
 plotOpticalSystem('newFigure',false,'surfaceSet',sceneGeometry.refraction.retinaToStop);
 plot3(X(:,1),X(:,2),X(:,3),'.r');
 
+smallEyeRadiusMm = mean(vecnorm((X-mean(X))')');
 
 title('TOME_3031, 3043, SR=-10, 4, axLength=27.57, 21.79');
 set(gca,'color','none')
 drawnow
-filename =fullfile(saveDir,'fig2','d');
+filename =fullfile(saveDir,'fig2','d_eye');
 vecrast(gcf, filename, 600, 'bottom', 'pdf')
 close(gcf)
+
+
+fig = figure;
+fig.Renderer='Painters';
+thickA = 0.5;
+
+[X,Y,Z] = cylinder(bigEyeRadiusMm,100);
+Z = Z*thickA;
+
+h = surf(X,Y,Z,'facecolor','b','LineStyle','none');
+h.FaceAlpha = 0.25;
+hold on
+axis equal
+h = fill3(X(1,:),Y(1,:),Z(1,:),'-b');
+h.FaceAlpha = 0.25;
+h = fill3(X(2,:),Y(2,:),Z(2,:),'-b');
+h.FaceAlpha = 0.25;
+
+V = pi * bigEyeRadiusMm^2 * thickA;
+thickB = V / (pi * smallEyeRadiusMm^2);
+
+[X,Y,Z] = cylinder(smallEyeRadiusMm,100);
+Z = Z*thickB;
+
+h = surf(X,Y,Z,'facecolor','r','LineStyle','none');
+h.FaceAlpha = 0.25;
+hold on
+axis equal
+%h = fill3(X(1,:),Y(1,:),Z(1,:),'r');
+%h.FaceAlpha = 0.25;
+h = fill3(X(2,:),Y(2,:),Z(2,:),'-r');
+h.FaceAlpha = 0.25;
+
+view(-36,26)
+axis off
+title('retinal cyliners');
+set(gca,'color','none')
+drawnow
+filename =fullfile(saveDir,'fig2','d_cylinder.pdf');
+
+setTightFig
+saveas(fig,filename);
+
+
 
 end
