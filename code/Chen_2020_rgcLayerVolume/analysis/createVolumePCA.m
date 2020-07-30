951187c43d36f0403eeb7ce213a072ecbd1d32ac
gkaguirre <gkaguirre@me.com>
2020-07-30 17:56:44 -0400

    BF: smoothness factor was not being used
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/Chen_2020_rgcLayerVolume/analysis/createVolumePCA.m
+++ code/Chen_2020_rgcLayerVolume/analysis/createVolumePCA.m
@@ -31,7 +31,6 @@ end
 
 % Perform piece-wise spline smoothing of the scores to remove the noisy
 % effects of data imputation
-smoothPCAFactor = 0.1; % 0-1, lower is smoother.
 GCVolPCAScoreExpandedSmoothed = GCVolPCAScoreExpanded;
 for cc = 1:size(GCVolPCAScoreExpanded,2)
     for dd = 1:length(domains)
