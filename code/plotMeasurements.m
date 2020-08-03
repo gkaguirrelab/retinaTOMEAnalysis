3955b3e49505aedb285b022fbf5ad48937e0fb71
Briana Haggerty <eddaha@alumni.upenn.edu>
2020-08-03 18:53:37 -0400

    plotMeasurements
    started a script to plot measurements for a subject from the four meridians all on the same x-axis of eccentricity values

---
+++ code/plotMeasurements.m
@@ -0,0 +1,47 @@
+%% plotMeasurements
+
+% This script plots the measurements for a subject from the four meridians
+% (nasal, temporal, superior, inferior) all on the same x-axis of
+% eccentricity values.
+
+% Obtain the dropBox base directory
+dropboxBaseDir = getpref('retinaTOMEAnalysis','dropboxBaseDir');
+
+%% load & place data
+
+% Find and load the files we need
+horizThicknessProfile = fullfile(dropboxBaseDir, 'AOSO_analysis', ...
+    'OCTExplorerExtendedHorizontalData', 'GCIP_thicknessesByDeg.mat');
+load(horizThicknessProfile,'thicknessMmHoriz','XPos_Degs');
+
+% Place data into a structure
+
+thickData(1).label = 'temporal';
+thickData(1).angle = 180;
+thickData(1).supportDeg = abs(XPos_Degs(XPos_Degs<=0));
+thickData(1).thickMM = (thicknessMmHoriz(XPos_Degs<=0)')';
+
+thickData(2).label = 'nasal';
+thickData(2).angle = 0;
+thickData(2).supportDeg = abs(XPos_Degs(XPos_Degs>=0));
+thickData(2).thickMM = (thicknessMmHoriz(XPos_Degs>=0)')';
+
+% Find and load the files we need
+vertThicknessProfile = fullfile(dropboxBaseDir, 'AOSO_analysis', ...
+    'OCTSingleVerticalData', 'GCIP_thicknessesByDeg.mat');
+load(vertThicknessProfile,'thicknessMmVert','XPos_Degs');
+
+% Place data into a structure
+
+thickData(3).label = 'superior';
+thickData(3).angle = 90;
+thickData(3).supportDeg = abs(XPos_Degs(XPos_Degs<=0));
+thickData(3).thickMM = (thicknessMmVert(XPos_Degs<=0)')';
+
+thickData(4).label = 'inferior';
+thickData(4).angle = 270;
+thickData(4).supportDeg = abs(XPos_Degs(XPos_Degs>=0));
+thickData(4).thickMM = (thicknessMmVert(XPos_Degs>=0)')';
+
+%% plot measurements
+plot(XPos_Degs, sqeeze(GCthicknessValuesAtXPos_um(1,1,:)))
