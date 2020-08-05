053e94709b2e0bfaaee769f192716987e376934d
Briana Haggerty <eddaha@alumni.upenn.edu>
2020-08-05 16:13:08 -0400

    syntax edits

--- code/plotMeasurements.m
+++ code/plotMeasurements.m
@@ -24,38 +24,16 @@ horizThicknessProfile = fullfile(dropboxBaseDir, 'AOSO_analysis', ...
 load(horizThicknessProfile,'GCthicknessValuesAtXPos_um', ...
     'IPthicknessValuesAtXPos_um', 'XPos_Degs');
 
-% place horizontal GC thickness data into a structure
-GCthickData(1).label = 'temporal';
-GCthickData(1).angle = 180;
-GCthickData(1).supportDeg = abs(XPos_Degs(XPos_Degs<=0));
-GCthickData(1).thickMM = (GCthicknessValuesAtXPos_um(XPos_Degs<=0)')';
-
-GCthickData(2).label = 'nasal';
-GCthickData(2).angle = 0;
-GCthickData(2).supportDeg = abs(XPos_Degs(XPos_Degs>=0));
-GCthickData(2).thickMM = (GCthicknessValuesAtXPos_um(XPos_Degs>=0)')';
-
 % plot horizontal GC thickness
-avgGC = mean(GCthicknessValuesAtXPos_um, 'omitnan')
-avgGC = mean(avgGC, 'omitnan')
+avgGC = mean(GCthicknessValuesAtXPos_um, 'omitnan');
+avgGC = mean(avgGC, 'omitnan');
 subplot(2,2,1)
 plot(XPos_Degs, squeeze(avgGC))
 hold on
 
-% place horizontal IP thickness data into a structure
-IPthickData(1).label = 'temporal';
-IPthickData(1).angle = 180;
-IPthickData(1).supportDeg = abs(XPos_Degs(XPos_Degs<=0));
-IPthickData(1).thickMM = (IPthicknessValuesAtXPos_um(XPos_Degs<=0)')';
-
-IPthickData(2).label = 'nasal';
-IPthickData(2).angle = 0;
-IPthickData(2).supportDeg = abs(XPos_Degs(XPos_Degs>=0));
-IPthickData(2).thickMM = (IPthicknessValuesAtXPos_um(XPos_Degs>=0)')';
-
 % plot horizontal IP thickness
-avgIP = mean(IPthicknessValuesAtXPos_um, 'omitnan')
-avgIP = mean(avgIP, 'omitnan')
+avgIP = mean(IPthicknessValuesAtXPos_um, 'omitnan');
+avgIP = mean(avgIP, 'omitnan');
 plot(XPos_Degs, squeeze(avgIP))
 title('Average Thickness Along the Horizontal Meridian')
 legend('GCL','IPL')
@@ -67,7 +45,10 @@ hold off
 
 % plot horizontal GC:GCIP thickness
 subplot(2,2,3)
-plot(XPos_Degs, squeeze(avgGC) ./ (squeeze(avgIP) + squeeze(avgGC)))
+avgGCIP = GCthicknessValuesAtXPos_um + IPthicknessValuesAtXPos_um;
+avgGCIP = mean(avgGCIP, 'omitnan');
+avgGCIP = mean(avgGCIP, 'omitnan');
+plot(XPos_Degs, squeeze(avgGC ./ avgGCIP))
 title('Average GCL Ratio Along the Horizontal Meridian')
 legend('GC:GCIP')
 xlabel('Eccentricity') 
@@ -77,44 +58,23 @@ ylim([0 .75])
 
 
 %% plot GCIP vertical thickness
+
 % Find and load the files we need across the vertical meridian
 vertThicknessProfile = fullfile(dropboxBaseDir, 'AOSO_analysis', ...
     'OCTSingleVerticalData', 'GCIP_thicknessesByDeg.mat');
 load(vertThicknessProfile,'GCthicknessValuesAtXPos_um', ...
     'IPthicknessValuesAtXPos_um', 'XPos_Degs');
 
-% place vertical GC data into a structure
-GCthickData(3).label = 'superior';
-GCthickData(3).angle = 90;
-GCthickData(3).supportDeg = abs(XPos_Degs(XPos_Degs<=0));
-GCthickData(3).thickMM = (GCthicknessValuesAtXPos_um(XPos_Degs<=0)')';
-
-GCthickData(4).label = 'inferior';
-GCthickData(4).angle = 270;
-GCthickData(4).supportDeg = abs(XPos_Degs(XPos_Degs>=0));
-GCthickData(4).thickMM = (GCthicknessValuesAtXPos_um(XPos_Degs>=0)')';
-
 % plot vertical GC thickness
-avgGC = mean(GCthicknessValuesAtXPos_um, 'omitnan')
-avgGC = mean(avgGC, 'omitnan')
+avgGC = mean(GCthicknessValuesAtXPos_um, 'omitnan');
+avgGC = mean(avgGC, 'omitnan');
 subplot(2,2,2)
 plot(XPos_Degs, squeeze(avgGC))
 hold on
 
-% place vertical IP into a structure
-IPthickData(3).label = 'superior';
-IPthickData(3).angle = 90;
-IPthickData(3).supportDeg = abs(XPos_Degs(XPos_Degs<=0));
-IPthickData(3).thickMM = (IPthicknessValuesAtXPos_um(XPos_Degs<=0)')';
-
-IPthickData(4).label = 'inferior';
-IPthickData(4).angle = 270;
-IPthickData(4).supportDeg = abs(XPos_Degs(XPos_Degs>=0));
-IPthickData(4).thickMM = (IPthicknessValuesAtXPos_um(XPos_Degs>=0)')';
-
 % plot vertical IP thickness
-avgIP = mean(IPthicknessValuesAtXPos_um, 'omitnan')
-avgIP = mean(avgIP, 'omitnan')
+avgIP = mean(IPthicknessValuesAtXPos_um, 'omitnan');
+avgIP = mean(avgIP, 'omitnan');
 plot(XPos_Degs, squeeze(avgIP))
 title('Average Thickness Along the Vertical Meridian')
 legend('GCL','IPL')
@@ -126,7 +86,10 @@ hold off
 
 % plot vertical GC:GCIP thickness
 subplot(2,2,4)
-plot(XPos_Degs, squeeze(avgGC) ./ (squeeze(avgIP) + squeeze(avgGC)))
+avgGCIP = GCthicknessValuesAtXPos_um + IPthicknessValuesAtXPos_um;
+avgGCIP = mean(avgGCIP, 'omitnan');
+avgGCIP = mean(avgGCIP, 'omitnan');
+plot(XPos_Degs, squeeze(avgGC ./ avgGCIP))
 title('Average GCL Ratio Along the Vertical Meridian')
 legend('GC:GCIP')
 xlabel('Eccentricity') 
