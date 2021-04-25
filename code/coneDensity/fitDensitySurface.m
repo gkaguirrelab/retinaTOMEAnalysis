38be9c6da1acbe0b4811d55c6f08abe4e60366f0
gkaguirre <gkaguirre@me.com>
2021-04-25 16:09:28 -0400

    Stand alone model fit function

--- code/coneDensity/fitDensitySurface.m
+++ code/coneDensity/fitDensitySurface.m
@@ -1,226 +1,150 @@
+function [p, Yfit, fVal] = fitDensitySurface(Y,w,preFitAvgEccen,simplePolarModel,p0,supportDeg,maxSupportDeg)
+% Fit a multi-parameter surface to cone density data
+%
+% Syntax:
+%   output = myFunc(input)
+%
+% Description:
+%   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+%   nulla a tempor scelerisque. Maecenas et lobortis est. Donec et turpis
+%   sem. Sed fringilla in metus ut malesuada. Pellentesque nec eros
+%   efficitur, pellentesque nisl vel, dapibus felis. Morbi eu gravida enim.
+%   Sed sodales ipsum eget finibus dapibus. Fusce sagittis felis id orci
+%   egestas, non convallis neque porttitor. Proin ut mi augue. Cras posuere
+%   diam at purus dignissim, vel vestibulum tellus ultrices
+%
+% Inputs:
+%   none
+%   foo                   - Scalar. Foo foo foo foo foo foo foo foo foo foo
+%                           foo foo foo foo foo foo foo foo foo foo foo foo
+%                           foo foo foo
+%
+% Optional key/value pairs:
+%   none
+%  'bar'                  - Scalar. Bar bar bar bar bar bar bar bar bar bar
+%                           bar bar bar bar bar bar bar bar bar bar bar bar
+%                           bar bar bar bar bar bar
+%
+% Outputs:
+%   none
+%   baz                   - Cell. Baz baz baz baz baz baz baz baz baz baz
+%                           baz baz baz baz baz baz baz baz baz baz baz baz
+%                           baz baz baz
+%
+% Examples:
+%{
+%}
 
 
-% The overal result directory
-cd('/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/Connectome_AOmontages_images')
-
-
-
-% Load the confocal and split data
-splitFiles=dir('densityAnalysis/*_split.mat');
-splitNames = strrep(extractfield(splitFiles,'name'),'_split.mat','');
-
-confocalFiles=dir('densityAnalysis/*_confocal.mat');
-confocalNames = strrep(extractfield(confocalFiles,'name'),'_confocal.mat','');
-
-supportLength = 1799;
-maxSupportDeg = 15;
-supportDegDelta = 0.0078;
-conStart = 0.5;
-conStop = 1.5;
-splitStart = 1.75;
-nFourier = 3;
-
-
-% Define the eccentricity support, and the ranges (in degrees) that will be
-% used for the confocal and split detecton data sets
-supportDeg = 0:supportDegDelta:supportDegDelta*(supportLength-1);
-idxA = find(supportDeg>=conStart,1);
-idxB = find(supportDeg>=conStop,1);
-idxC = find(supportDeg>=splitStart,1);
-
-
-%% Loop through subjects and create the composite polar density image
-
-subNames = unique([splitNames confocalNames]);
-dataMat = nan(supportLength,supportLength,length(subNames));
-
-for ss = 1:length(subNames)
-    
-    % A matrix to hold the data for this subject
-    y = nan(supportLength,supportLength);
-    
-    % Add the confocal data
-    conFile = fullfile('densityAnalysis',[subNames{ss} '_confocal.mat']);
-    if isfile(conFile)
-        load(conFile,'data');
-        y(:,idxA:idxB) = data.polarDensity(:,idxA:idxB);
-    end
-    
-    % Add the split data
-    splitFile = fullfile('densityAnalysis',[subNames{ss} '_split.mat']);
-    if isfile(splitFile)
-        load(splitFile,'data');
-        y(:,idxC:end) = data.polarDensity(:,idxC:end);
-    end
-    
-    % Filter out any negative values
-    y(y<0)=nan;
-    dataMat(:,:,ss)=y;
-    
+arguments
+    Y (:,:) {mustBeNumeric}
+    w (:,:) {mustBeNumeric} = ones(size(Y))
+    preFitAvgEccen (1,1) = true;
+    simplePolarModel (1,1) = true;
+    p0 (1,20) {mustBeNumeric} = [1.4219e+03, -0.0813, 9.4727e+03, -1.2994, 18.9946, -0.0663, 10.1278, 0.1424, 0, -0.0627, 6.8191, 0.8296, -10.8437, 0.0925, 3.9524, 0.4707, -3.6584, 0.1173, 2.9143, 1.999]
+    supportDeg (1,:) {mustBeNumeric} = 0:0.0078:0.0078*(size(Y,1)-1)
+    maxSupportDeg (1,1) {mustBeNumeric} = 15
 end
 
-polarRatio = (size(dataMat,1)+1)/360;
+%% pBlock and mBlock settings
+% These are the parameters of the model. The pBlock defines the form of the
+% sum of exponential fit. The mBlock defines the parameters of a gamma
+% function modulation of a sinusoidal variation in density across polar
+% angle.
+
+pBlockLB = [0,-5,0,-5];
+pBlockUB = [5e4,0,5e4,0];
+
+mBlockLB = [-35 -1 2 0.01];
+mBlockUB = [35 1 20 2];
+
+% The number of Fourier components that models variation across polar angle
+nFourier = 4;
+
+% The length of the support, pulled out here for code clarity
+supportLength = length(supportDeg);
+
 
 %% Initial fit to mean density profile
 
-% Density, eccentricity, polar angle
-Y = nanmean(nanmean(dataMat,3));
-w = nanmean(sum(~isnan(dataMat),3));
-X = supportDeg;
-P = zeros(size(X));
+if preFitAvgEccen
 
-% objective
-validIdx = ~isnan(Y);
-myObj = @(p) norm( w(validIdx).* (Y(validIdx) - myModel(X(validIdx),P(validIdx),maxSupportDeg,p)) );
+    % Density, eccentricity, polar angle
+    Ym = nanmean(Y,1);
+    wm = nanmean(w,1);
+    X = supportDeg;
+    P = zeros(size(X))+90;
+    
+    % Bounds
+    lb = [pBlockLB, p0(5:end)];
+    ub = [pBlockUB, p0(5:end)];
 
-% p0 and bounds
-asymptote = 0; % In case we want to force a minimum asymptote value
-pBlock0 = [1300, -0.25, 8500, -1.25, asymptote];
-pBlockLB = [0,-5,0,-5,asymptote];
-pBlockUB = [5e4,0,5e4,0,asymptote];
-mBlock0 = [0 0 1.01 1.01];
+    % Options
+    options = optimoptions('fmincon','Display','off','Algorithm','interior-point');
 
-p0 = [pBlock0, repmat(mBlock0,1,nFourier)];
-lb = [pBlockLB, repmat(mBlock0,1,nFourier)];
-ub = [pBlockUB, repmat(mBlock0,1,nFourier)];
+    % objective
+    validIdx = ~isnan(Ym);
+    myObj = @(p) norm( wm(validIdx).* (Ym(validIdx) - myModel(X(validIdx),P(validIdx),maxSupportDeg,p)) );
+    
+    % search
+    pm = fmincon(myObj,p0,[],[],[],[],lb,ub,[],options);
+    
+    % Update p0
+    p0(1:4) = pm(1:4);
 
-% search
-p = fmincon(myObj,p0,[],[],[],[],lb,ub);
+end
 
 
 %% Fit polar angle variation
 
 % Density, eccentricity, polar angle
-Y = nanmean(dataMat,3);
-w = sum(~isnan(dataMat),3);
 X = repmat(supportDeg,supportLength,1);
 P = repmat(linspace(0,360,supportLength)',1,supportLength);
-
-% objective
 validIdx = ~isnan(Y);
-myObj = @(p) norm( w(validIdx).* (Y(validIdx) - myModel(X(validIdx),P(validIdx),maxSupportDeg,p)) );
 
-% p0 and bounds
-mBlockLB = [-30 -1 2 0.01];
-mBlockUB = [30 1 20 4];
-
-p0 = [p(1:5), repmat(mBlock0,1,nFourier)];
-lb = [pBlockLB, repmat(mBlockLB,1,nFourier)];
-ub = [pBlockUB, repmat(mBlockUB,1,nFourier)];
+if simplePolarModel
+    
+    % p0 and bounds
+    lb = [pBlockLB, -30 0];
+    ub = [pBlockUB, 30 2];
+    
+    % assemble p function
+    pFull = @(p) [p(1:4) p0(5)+p(5) p0(6)*p(6) p0(7:8) p0(9)+p(5) p0(10)*p(6) p0(11:12) p0(13)+p(5) p0(14)*p(6) p0(15:16) p0(17)+p(5) p0(18)*p(6) p0(19:20)];
+    
+    % truncate p0
+    p0 = [p0(1:4) 0 1];
+    
+    % objective
+    myObj = @(p) norm( w(validIdx).* (Y(validIdx) - myModel(X(validIdx),P(validIdx),maxSupportDeg,pFull(p))) );
+    
+else
+    
+    % p0 and bounds
+    lb = [pBlockLB, repmat(mBlockLB,1,nFourier)];
+    ub = [pBlockUB, repmat(mBlockUB,1,nFourier)];
+    
+    % objective
+    myObj = @(p) norm( w(validIdx).* (Y(validIdx) - myModel(X(validIdx),P(validIdx),maxSupportDeg,p)) );
+    
+end
 
 % search
-p = fmincon(myObj,p0,[],[],[],[],lb,ub);
+options = optimoptions('fmincon','Display','off','Algorithm','interior-point','UseParallel',true);
+[p, fVal] = fmincon(myObj,p0,[],[],[],[],lb,ub,[],options);
 
-
-%% Fit an individual subject
-
-% Data
-Y = squeeze(dataMat(:,:,3));
-
-% objective
-validIdx = ~isnan(Y);
-myObj = @(p) norm( Y(validIdx) - myModel(X(validIdx),P(validIdx),maxSupportDeg,p) );
-
-% p0 and bounds
-mBlockFit = p(6:end);
-p0 = [p(1:5), mBlockFit];
-lb = [pBlockLB, mBlockFit];
-ub = [pBlockUB, mBlockFit];
-
-% search
-pSub1 = fmincon(myObj,p0,[],[],[],[],lb,ub);
-
-foo=1;
-
-
-
-%% plot
+if simplePolarModel
+    p = pFull(p);
+end
 
 % generate the model fit
 Yfit = nan(supportLength,supportLength);
 Yfit(:,:)=myModel(X,P,maxSupportDeg,p);
 
-meridianLabels = {'Nasal','Superior','Temporal','Inferior','Nasal'};
-meridianAngles = [0 90 180 270 360];
 
-figure
-surf(X,P,Yfit,'FaceAlpha',0.5,'EdgeColor','none');
-hold on
-plot3(X(:),P(:),Y(:),'.k')
-yticks(meridianAngles);
-yticklabels(meridianLabels);
-xlabel('Eccentricity [deg]');
-zlabel('Density [cones/deg^2]');
-
-
-figure
-for ii = [0.375 0.75 1.5 3 6 10]
-    idx = find(supportDeg>ii,1);
-    semilogy(Y(:,idx),'.');
-    hold on
-    semilogy(Yfit(:,idx),'-r');
-    text(300*polarRatio,Yfit(45*polarRatio,idx),sprintf('%2.1f°',supportDeg(idx)));
-end
-xticks(meridianAngles*polarRatio);
-xticklabels(meridianLabels);
-ylim([10^2,10^4]);
-ylabel('log_1_0 density [cones/deg^2]')
-
-
-figure
-for mm=1:4
-    subplot(2,2,mm)
-    plot(supportDeg,Y(round((meridianAngles(mm))*polarRatio+1),:),'.k');
-    hold on
-    plot(supportDeg,Yfit(round((meridianAngles(mm))*polarRatio+1),:),'-r');
-    xlabel('Eccentricity [deg]');
-    ylabel('Density [cones/deg^2]');
-    title(meridianLabels{mm});
 end
 
-figure
-meridianSpec = {'-r','--b','-m','--g'};
-for mm=1:4
-    plot(supportDeg,Yfit(meridianAngles(mm)*polarRatio+1,:),meridianSpec{mm},'LineWidth',2);
-    hold on
-end
-xlabel('Eccentricity [deg]');
-ylabel('Density [cones/deg^2]');
-legend(meridianLabels(1:4));
 
-
-figure
-for gg = 1:nFourier
-    ph = p((gg-1)*4+6);
-    f1 = p((gg-1)*4+7);
-    f2 = p((gg-1)*4+8);
-    f3 = p((gg-1)*4+9);
-    g = f1.*gampdf(supportDeg,f2,f3)./max(gampdf(0:0.01:maxSupportDeg,f2,f3));
-    plot(supportDeg,g)
-    hold on
-end
-lengendLabels = {'cos1','cos2','cos4'};
-legend(lengendLabels(1:nFourier));
-xlabel('Eccentricity [deg]');
-ylabel('Modulation [a.u.]');
-
-
-figure
-imagesc(w)
-axis square
-yticks(meridianAngles*polarRatio);
-yticklabels(meridianLabels);
-deltaDeg = supportDeg(2)-supportDeg(1);
-xticks(round(0:1/deltaDeg:floor(maxSupportDeg)/deltaDeg));
-xticklabels(0:1:floor(maxSupportDeg));
-xlabel('Eccentricity [deg]');
-colorbar
-title('Weight map');
-
-
-foo=1;
-
-
+%% Local functions
 
 function density = myModel(x,angle,maxX,p)
 
@@ -228,29 +152,35 @@ a = p(1);
 b = p(2);
 c = p(3);
 d = p(4);
-e = p(5);
-ph1 = p(6);
-f11 = p(7);
-f12 = p(8);
-f13 = p(9);
-ph2 = p(10);
-f21 = p(11);
-f22 = p(12);
-f23 = p(13);
-ph3 = p(14);
-f31 = p(15);
-f32 = p(16);
-f33 = p(17);
+ph1 = p(5);
+f11 = p(6);
+f12 = p(7);
+f13 = p(8);
+ph2 = p(9); % Unused params. The sin1 has the same angle as cos1
+f21 = p(10);
+f22 = p(11);
+f23 = p(12);
+ph3 = p(13);
+f31 = p(14);
+f32 = p(15);
+f33 = p(16);
+ph4 = p(17);
+f41 = p(18);
+f42 = p(19);
+f43 = p(20);
+
 
 g1 = f11.*gampdf(x,f12,f13)./max(gampdf(0:0.01:maxX,f12,f13));
 g2 = f21.*gampdf(x,f22,f23)./max(gampdf(0:0.01:maxX,f22,f23));
 g3 = f31.*gampdf(x,f32,f33)./max(gampdf(0:0.01:maxX,f32,f33));
+g4 = f41.*gampdf(x,f42,f43)./max(gampdf(0:0.01:maxX,f42,f43));
 
 m = g1.*cosd(angle+ph1) + ...
-    g2.*cosd(angle.*2+ph2) + ...
-    g3.*cosd(angle.*4+ph3);
+    g2.*sind(angle+ph1) + ...
+    g3.*cosd(angle.*2+ph3) + ...
+    g4.*cosd(angle.*4+ph4);
 
-density = (m+1).*(a.*exp(b.*x)+c.*exp(d.*x)+e);
+density = (m+1).*(a.*exp(b.*x)+c.*exp(d.*x));
 
 end
 
---
+++ code/coneDensity/fitScript.m
@@ -0,0 +1,162 @@
+
+
+% The overal result directory
+cd('/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/Connectome_AOmontages_images')
+
+
+% Load the confocal and split data
+splitFiles=dir('densityAnalysis/*_split.mat');
+splitNames = strrep(extractfield(splitFiles,'name'),'_split.mat','');
+
+confocalFiles=dir('densityAnalysis/*_confocal.mat');
+confocalNames = strrep(extractfield(confocalFiles,'name'),'_confocal.mat','');
+
+supportLength = 1799;
+maxSupportDeg = 15;
+supportDegDelta = 0.0078;
+conStart = 0.5;
+conStop = 1.5;
+splitStart = 1.75;
+
+% Define the eccentricity support, and the ranges (in degrees) that will be
+% used for the confocal and split detecton data sets
+supportDeg = 0:supportDegDelta:supportDegDelta*(supportLength-1);
+idxA = find(supportDeg>=conStart,1);
+idxB = find(supportDeg>=conStop,1);
+idxC = find(supportDeg>=splitStart,1);
+
+
+%% Loop through subjects and create the composite polar density image
+
+subNames = unique([splitNames confocalNames]);
+dataMat = nan(supportLength,supportLength,length(subNames));
+
+for ss = 1:length(subNames)
+    
+    % A matrix to hold the data for this subject
+    y = nan(supportLength,supportLength);
+    
+    % Add the confocal data
+    conFile = fullfile('densityAnalysis',[subNames{ss} '_confocal.mat']);
+    if isfile(conFile)
+        load(conFile,'data');
+        y(:,idxA:idxB) = data.polarDensity(:,idxA:idxB);
+    end
+    
+    % Add the split data
+    splitFile = fullfile('densityAnalysis',[subNames{ss} '_split.mat']);
+    if isfile(splitFile)
+        load(splitFile,'data');
+        y(:,idxC:end) = data.polarDensity(:,idxC:end);
+    end
+    
+    % Filter out any negative values
+    y(y<0)=nan;
+    dataMat(:,:,ss)=y;
+    
+end
+
+
+% Fit the mean
+Y = nanmean(dataMat,3);
+w = sum(~isnan(dataMat),3);
+[p, Yfit, fVal] = fitDensitySurface(Y,w,true,false);
+
+% Loop through and fit each subject with the reduced model
+pSet = nan(20,length(subNames));
+YfitSet = nan(size(dataMat));
+fValSet= nan(1,length(subNames));
+
+fprintf('fitting...');
+for ii = 1:length(subNames)
+    Y = squeeze(dataMat(:,:,ii));
+    fprintf([num2str(ii),'...']);
+    [pSet(:,ii), YfitSet(:,:,ii), fValSet(ii)] = fitDensitySurface(Y);
+end
+fprintf('done\n');
+
+
+%% plot
+
+% Values needed for the plots
+polarRatio = (size(dataMat,1)+1)/360;
+nFourier = 4;
+meridianLabels = {'Nasal','Superior','Temporal','Inferior','Nasal'};
+meridianAngles = [0 90 180 270 360];
+
+figure
+X = repmat(supportDeg,supportLength,1);
+P = repmat(linspace(0,360,supportLength)',1,supportLength);
+surf(X,P,Yfit,'FaceAlpha',0.5,'EdgeColor','none');
+hold on
+plot3(X(:),P(:),Y(:),'.k')
+yticks(meridianAngles);
+yticklabels(meridianLabels);
+xlabel('Eccentricity [deg]');
+zlabel('Density [cones/deg^2]');
+
+
+figure
+for ii = [0.375 0.75 1.5 3 6 10]
+    idx = find(supportDeg>ii,1);
+    semilogy(Y(:,idx),'.');
+    hold on
+    semilogy(Yfit(:,idx),'-r');
+    text(300*polarRatio,Yfit(45*polarRatio,idx),sprintf('%2.1f°',supportDeg(idx)));
+end
+xticks(meridianAngles*polarRatio);
+xticklabels(meridianLabels);
+ylim([10^2,10^4]);
+ylabel('log_1_0 density [cones/deg^2]')
+
+
+figure
+for mm=1:4
+    subplot(2,2,mm)
+    plot(supportDeg,Y(round((meridianAngles(mm))*polarRatio+1),:),'.k');
+    hold on
+    plot(supportDeg,Yfit(round((meridianAngles(mm))*polarRatio+1),:),'-r');
+    xlabel('Eccentricity [deg]');
+    ylabel('Density [cones/deg^2]');
+    title(meridianLabels{mm});
+end
+
+figure
+meridianSpec = {'-r','--b','-m','--g'};
+for mm=1:4
+    semilogy(supportDeg,Yfit(meridianAngles(mm)*polarRatio+1,:),meridianSpec{mm},'LineWidth',2);
+    hold on
+end
+xlabel('Eccentricity [deg]');
+ylabel('Density [cones/deg^2]');
+legend(meridianLabels(1:4));
+
+
+figure
+for gg = 1:nFourier
+    ph = p((gg-1)*4+5);
+    f1 = p((gg-1)*4+6);
+    f2 = p((gg-1)*4+7);
+    f3 = p((gg-1)*4+8);
+    g = f1.*gampdf(supportDeg,f2,f3)./max(gampdf(0:0.01:maxSupportDeg,f2,f3));
+    plot(supportDeg,g)
+    hold on
+end
+lengendLabels = {'cos1','sin1','cos2','cos4'};
+legend(lengendLabels(1:nFourier));
+xlabel('Eccentricity [deg]');
+ylabel('Modulation [a.u.]');
+
+
+figure
+imagesc(w)
+axis square
+yticks(meridianAngles*polarRatio);
+yticklabels(meridianLabels);
+deltaDeg = supportDeg(2)-supportDeg(1);
+xticks(round(0:1/deltaDeg:floor(maxSupportDeg)/deltaDeg));
+xticklabels(0:1:floor(maxSupportDeg));
+xlabel('Eccentricity [deg]');
+colorbar
+title('Weight map');
+
